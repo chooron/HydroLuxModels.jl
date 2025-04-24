0053c859eaca0f564914ec4cb3f2183ca4bba1f0
jingx <50790703+chooron@users.noreply.github.com>
2025-04-24 12:08:25 +0000

    update exphydro

--- dev/test_exphydro.jl
+++ dev/test_exphydro.jl
@@ -3,31 +3,49 @@ using Lux
 using Reactant, Enzyme
 using Random
 using BenchmarkTools
+using ComponentArrays
+using Optimisers
+using Printf
+using MLUtils: unsqueeze
 
 include("../src/HydroLuxModels.jl")
+Reactant.set_default_backend("cpu")
 
 hidden_dims = 10
+rng = Random.default_rng()
 dev = reactant_device()
 model = HydroLuxModels.ExpHydroLuxModel(hidden_dims)
-ps, st = Lux.setup(Random.default_rng(), model) |> dev
+ps, st = Lux.setup(rng, model) |> dev
 
-tmp_input = rand(Float32, 3, hidden_dims, 1000) |> dev
-
+# load data
+file_path = "data/01013500.csv"
+forcing_data = CSV.File(file_path) |> DataFrame
+x_arr = forcing_data[!, [Symbol("prcp(mm/day)"), Symbol("tmean(C)"), Symbol("dayl(day)")]] |> Matrix |> permutedims
+x_data = repeat(unsqueeze(x_arr, dims=2), 1, hidden_dims, 1) |> dev
+y_data = forcing_data[!, [Symbol("flow(mm)")]] |> Matrix |> permutedims |> dev
+tmp_input = rand(3, 10, 1000) |> dev
 model_compiled = @compile sync = true model(tmp_input, ps, st)
+# # All of this can be automated using the TrainState API
+# train_state = Training.TrainState(model, ps, st, Adam(0.01f0))
+# model_compiled = @compile sync = true model(x_data, ps, st)
+# output = model_compiled(tmp_input, ps, st)  # 0.005492 seconds (15 allocations: 544 bytes)
+# compiled_grad = @compile sync = true Enzyme.gradient(
+#     Reverse, Const(sum ∘ first ∘ model), Const(tmp_input), ps, Const(st)
+# )
 
-@btime output = model_compiled(tmp_input, ps, st)  # 0.005492 seconds (15 allocations: 544 bytes)
-compiled_grad = @compile sync = true Enzyme.gradient(
-    Reverse, Const(sum ∘ first ∘ model), Const(tmp_input), ps, Const(st)
-)
+# function train_model!(model, ps, st, x_data, y_data)
+#     train_state = Lux.Training.TrainState(model, ps, st, Adam(0.01f0))
+#     for iter in 1:1000
+#         _, loss, _, train_state = Lux.Training.single_train_step!(
+#             AutoEnzyme(), MSELoss(),
+#             (x_data, y_data), train_state
+#         )
+#         if iter % 100 == 1 || iter == 1000
+#             @printf "Iteration: %04d \t Loss: %10.9g\n" iter loss
+#         end
+#     end
 
-compiled_grad(
-    Reverse, Const(sum ∘ first ∘ model), Const(tmp_input), ps, Const(st)
-) # 0.019394 seconds (47 allocations: 1.328 KiB)
+#     return model, ps, st
+# end
 
-# All of this can be automated using the TrainState API
-train_state = Training.TrainState(model, ps, st, Adam(0.01f0))
-
-gs, loss, stats, train_state = Training.single_train_step!(
-    AutoEnzyme(), MSELoss(),
-    (x, dev(rand(rng, Float32, 10, 2))), train_state
-)
+# train_model!(model, ps, st, x_data, y_data)
